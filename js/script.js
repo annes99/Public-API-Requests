47b2a66a7b3e36616462d0d95f28e398241d96d5
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-04-26 19:23:54 +0700

    add update JS  to meets req

--- js/script.js
+++ js/script.js
@@ -1,65 +1,116 @@
 const search = document.querySelector('.search-container');
-const gallery = document.getElementById('gallery');
+const gallery = document.querySelector('#gallery');
+let modal = document.createElement('div');
+const script = document.body.querySelector('script');
+document.body.insertBefore(modal, script);
+modal.setAttribute('class', 'modal-container');
+$('.modal-container').hide();
 
 
-fetch('https://randomuser.me/api/?results=12&nat=gb,us')
-.then(response => response.json())
-.then(data => generateGallery(data.results))
-//.then(data => generateModal(data.results))
-fetch('https://randomuser.me/api/?results=12&nat=gb,us')
-.then(response => response.json())
-//.then(data => generateSearch(data.results))
-.then(data => console.log(data.results));
+/*** Fetch Data From API ***/
 
-
+$.ajax({
+    url: 'https://randomuser.me/api/?results=12&nat=gb,us,au,nz,de',
+    dataType: 'json',
+    success: function (data) {
+        generateGallery(data.results);
+        generateModal(data.results);
+        //generateSearch(data.results); //still needs to be done
+    }
+});
+ 
 function generateGallery(data){
-    const html = data.map(item => `
-    <div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="${item.picture.medium}" alt="profile picture">
+    gallery.innerHTML = data.map(item => `
+        <div class="card" id="${item.dob.date}">
+            <div class="card-img-container">
+                <img class="card-img" src="${item.picture.medium}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
+                <p class="card-text">${item.email}</p>
+                <p class="card-text cap">${item.location.city}</p>
+            </div>
         </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
-            <p class="card-text">${item.email}</p>
-            <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
-        </div>
-    </div>
     `).join(''); 
-    gallery.innerHTML = html;
 }
+
 
 function generateModal(data){
-    const html = data.map(item => `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${item.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
-                    <p class="modal-text">${item.email}</p>
-                    <p class="modal-text cap">${item.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${item.cell}</p>
-                    <p class="modal-text">${item.location.street}, ${item.location.city}, ${item.location.state} ${item.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${item.dob.date}</p>
-                </div>
-            </div>
-            <div class="modal-btn-container">
-                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                <button type="button" id="modal-next" class="modal-next btn">Next</button>
-            </div>
-        </div> 
-    `).join(''); 
-    gallery.innerHTML = html;
+
+    // click on card => show modal
+    $('.card').on('click', function (event) {
+    
+        console.log(data);
+
+        const result = data.filter(function (entry) { return entry.dob.date === $(event.target).attr('id'); });
+        const profileData = result[0];
+    
+        createModalHTML(profileData);
+
+        $('.modal-container').show();
+
+        /* click events for modal window next prev buttons
+
+        $('#modal-next').on('click', () =>{
+            console.log('modal NEXT clicked');
+            createModalHTML(data[5]);
+            $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
+            });
+
+        $('#modal-prev').on('click', () =>{
+            console.log('modal PREV clicked');
+            createModalHTML(data[3]);
+            //$('.modal-container').hide();
+            $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
+        });
+        */
+
+        // closing hide modal window
+        $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
+    });
+    
 }
 
+// function to add modal window to index.html dynamically
+function createModalHTML (input) {
+    modal.innerHTML = `
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${input.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${input.name.first} ${input.name.last}</h3>
+                <p class="modal-text">${input.email}</p>
+                <p class="modal-text cap">${input.location.city}</p>
+                <hr>
+                <p class="modal-text">${input.phone}</p>
+                <p class="modal-text-street">${input.location.street}, ${input.location.city}, ${input.location.state} ${input.location.postcode}</p>
+                <p class="modal-text">Birthday: ${formatDate(input.dob.date)}</p>
+            </div>
+        </div>
+    `;
+}
+
+/***  Searchbar Functionality  (still needs to be done)***/
+
+// add searchbar
+/*
 function generateSearch(data){
-    const html = `
+    search.innerHTML  = `
         <form action="#" method="get">
             <input type="search" id="search-input" class="search-input" placeholder="Search...">
             <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
         </form> 
     `; 
-    search.innerHTML = html;
 }
+*/
 
+/*** Helper Functions  ***/
+
+// format date from yyyy-mm-ddThh:mm:ssZ => dd/mm/yyyy
+function formatDate(date) {
+    const input = new Date(date);
+    const day = input.getDate();
+    const month = input.getMonth() + 1;
+    const year = input.getFullYear();
+    return `${day <= 9 ? '0' + day : day}/${month<=9 ? '0' + month : month}/${year}`;
+}
