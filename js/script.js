f4ed6aa54060b19764093528a8b8275eb7a091a9
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-04-29 00:14:09 +0700

    add exceeds before cleanup

--- js/script.js
+++ js/script.js
@@ -1,116 +1,144 @@
 const search = document.querySelector('.search-container');
 const gallery = document.querySelector('#gallery');
-let modal = document.createElement('div');
+const modal = document.createElement('div');
 const script = document.body.querySelector('script');
 document.body.insertBefore(modal, script);
-modal.setAttribute('class', 'modal-container');
-$('.modal-container').hide();
+modal.setAttribute('id', 'modal');
 
 
 /*** Fetch Data From API ***/
+function getData(url){
+    return fetch(url)
+        .then(status)
+        .then(json)
+        .catch(function(error) {
+            console.log('Request failed', error);
+        });
+}
 
-$.ajax({
-    url: 'https://randomuser.me/api/?results=12&nat=gb,us,au,nz,de',
-    dataType: 'json',
-    success: function (data) {
-        generateGallery(data.results);
-        generateModal(data.results);
-        //generateSearch(data.results); //still needs to be done
-    }
-});
- 
-function generateGallery(data){
-    gallery.innerHTML = data.map(item => `
-        <div class="card" id="${item.dob.date}">
+
+getData('https://randomuser.me/api/?results=12&nat=gb,us,au,ca,nz&noinfo')
+  .then(data => data.results.forEach(result => {
+
+    gallery.innerHTML += `
+        <div class="card" id="${result.dob.date}">
             <div class="card-img-container">
-                <img class="card-img" src="${item.picture.medium}" alt="profile picture">
+                <img class="card-img" src="${result.picture.medium}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
-                <p class="card-text">${item.email}</p>
-                <p class="card-text cap">${item.location.city}</p>
+                <h3 id="name" class="card-name cap">${result.name.first} ${result.name.last}</h3>
+                <p class="card-text">${result.email}</p>
+                <p class="card-text cap">${result.location.city}</p>
             </div>
         </div>
-    `).join(''); 
-}
+    `; 
 
-
-function generateModal(data){
-
-    // click on card => show modal
-    $('.card').on('click', function (event) {
-    
-        console.log(data);
-
-        const result = data.filter(function (entry) { return entry.dob.date === $(event.target).attr('id'); });
-        const profileData = result[0];
-    
-        createModalHTML(profileData);
-
-        $('.modal-container').show();
-
-        /* click events for modal window next prev buttons
-
-        $('#modal-next').on('click', () =>{
-            console.log('modal NEXT clicked');
-            createModalHTML(data[5]);
-            $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
-            });
-
-        $('#modal-prev').on('click', () =>{
-            console.log('modal PREV clicked');
-            createModalHTML(data[3]);
-            //$('.modal-container').hide();
-            $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
-        });
-        */
-
-        // closing hide modal window
-        $('#modal-close-btn').on('click', () =>$('.modal-container').hide());
-    });
-    
-}
-
-// function to add modal window to index.html dynamically
-function createModalHTML (input) {
-    modal.innerHTML = `
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="${input.picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${input.name.first} ${input.name.last}</h3>
-                <p class="modal-text">${input.email}</p>
-                <p class="modal-text cap">${input.location.city}</p>
-                <hr>
-                <p class="modal-text">${input.phone}</p>
-                <p class="modal-text-street">${input.location.street}, ${input.location.city}, ${input.location.state} ${input.location.postcode}</p>
-                <p class="modal-text">Birthday: ${formatDate(input.dob.date)}</p>
+    modal.innerHTML += `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${result.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${result.name.first} ${result.name.last}</h3>
+                    <p class="modal-text">${result.email}</p>
+                    <p class="modal-text cap">${result.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${result.cell}</p>
+                    <p class="modal-text-street">${result.location.street}, ${result.location.city}, ${result.location.state} ${result.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${formatDate(result.dob.date)}</p>
+                </div>
+            </div>  
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
         </div>
     `;
-}
 
-/***  Searchbar Functionality  (still needs to be done)***/
+    $('.modal-container').hide();
 
-// add searchbar
-/*
-function generateSearch(data){
     search.innerHTML  = `
         <form action="#" method="get">
-            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+            <input name="q" type="search" id="search-input" class="search-input" placeholder="Search...">
             <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
         </form> 
     `; 
-}
-*/
+
+}));
+
+
+$(document).on('click', '.card', function (event) {
+    event.preventDefault();
+    let index = $(event.currentTarget).index();
+   $('#modal').children().eq(index).show();
+
+});
+
+
+let count = 1;
+$(document).on('click', '#modal-next', (event) =>{
+    event.preventDefault();
+    count++;
+    if(count > 12) {
+        count = 1;
+    } 
+    console.log(count);
+    $('.modal-container').hide();
+    $(`.modal-container:nth-child(${count})`).show();
+});
+
+$(document).on('click', '#modal-prev', (event) =>{
+    count--;
+    if(count < 1) {
+        count = 12;
+    }
+    $('.modal-container').hide();
+    $(`.modal-container:nth-child(${count})`).show();
+        
+});
+
+
+$(document).on('click', '#modal-close-btn', () =>$('.modal-container').hide());
+
+
+$(document).on('submit', function(event)  {
+    event.preventDefault();
+    const searchValue = $('#search-input').val().toLowerCase();
+
+    let value = $('#gallery').find(`.card:contains('${searchValue}')`).index();
+    if(value >= 0){
+        $('#gallery').children().hide();
+        $('#gallery').children().eq(value).show();
+    } 
+ 
+    else if (value < 0){
+        alert(`No mach for ${searchValue}`);
+        $('#gallery').children().show();
+    }
+   
+});
+
 
 /*** Helper Functions  ***/
 
-// format date from yyyy-mm-ddThh:mm:ssZ => dd/mm/yyyy
+// format date from json object yyyy-mm-ddThh:mm:ssZ to dd/mm/yyyy
 function formatDate(date) {
     const input = new Date(date);
     const day = input.getDate();
     const month = input.getMonth() + 1;
     const year = input.getFullYear();
-    return `${day <= 9 ? '0' + day : day}/${month<=9 ? '0' + month : month}/${year}`;
+    // if day or month value is less than 10 add 0 in front
+    return `${day <= 9 ? '0' + day : day}/${month <= 9 ? '0' + month : month}/${year}`;
+}
+
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+}
+  
+function json(response) {
+return response.json();
 }
